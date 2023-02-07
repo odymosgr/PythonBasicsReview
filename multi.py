46e444ddc30626223b3574b4d1c012208f3df8f1
Odysseas Moschidis <odysseasmoschidis@gmail.com>
2023-02-07 15:27:23 +0200

    multiprocessing

---
+++ multi.py
@@ -0,0 +1,26 @@
+from multiprocessing import Process
+import os
+import time
+
+def square_numbers():
+  for i in range(100):
+    i*i
+    time.sleep(0.1)
+
+processes=[]
+num_processes = os.cpu_count()
+print(num_processes)
+
+for i in range(num_processes):
+  p = Process(target=square_numbers)
+  processes.append(p)
+
+if __name__ == '__main__':
+
+  for p in processes:
+    p.start()
+
+  for p in processes:
+    p.join()
+
+  print("end main")
